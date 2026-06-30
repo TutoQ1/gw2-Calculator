4405bd63df134c6c8495d89ec7bac4570dfa8970
Nelson Leon <leonnelson447@gmail.com>
2026-06-30 01:31:59 -0500

    ❇️ Sumatory Function added

--- demo/src/main/java/org/example/demo/services/Implements/GoldSum.java
+++ demo/src/main/java/org/example/demo/services/Implements/GoldSum.java
@@ -1,29 +1,32 @@
 package org.example.demo.services.Implements;
 
 
-import org.example.demo.entity.Currency;
+import org.example.demo.entity.Valuables;
 import org.example.demo.services.InterValidations.Interface;
 
+import java.util.ArrayList;
+
 public class GoldSum implements Interface {
-    Currency currency = new Currency();
+
+    Valuables valuables = new Valuables();
+
     @Override
-    public void validation(Currency currency) throws Exception {
-        if(currency.getCopper()<0&&currency.getGold()<0&&currency.getSilver()<0){
+    public void validation(Valuables valuables) throws Exception {
+        if(valuables.getCopper()<0&& valuables.getGold()<0&& valuables.getSilver()<0){
             throw new Exception("Valor invalido");
         }
     }
-    private long value = 0;
+    long value = 0;
 
-    public void sumGold(double gold, double silver, double copper){
-
+    public void sumGold(double gold,double silver, double copper){
         gold *= 10000;
-        silver *= 100;
+        silver *=100;
 
-        value = (long) (gold+silver+copper);
+        value += (long) (gold+silver+copper);
     }
 
-    public Currency getSumGold(){
-        return currency.convertor(value);
+    public Valuables getSumGold(){
+        return valuables.conversion(value);
     }
     public void reset(){
         value = 0;
